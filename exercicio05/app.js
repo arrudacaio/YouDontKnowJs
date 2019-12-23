9732d96cb3bbf3b324643d06db2f44a2e4a129cb
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2019-12-23 04:26:10 -0300

    add lecture 56.

---
+++ exercicio05/app.js
@@ -0,0 +1,9 @@
+//Lecture 56. Using const with Arrays
+
+// The array can be changed as long as the reference is the same.
+// We can changed because we're not updating or altering this
+// reference.
+const myEggs = ['brown','brown'];
+myEggs[0] = 'amazing';
+myEggs.push('nice!');
+
