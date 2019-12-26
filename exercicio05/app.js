a232d85594458b29ae996592b8505c46f8cacb51
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2019-12-26 01:39:05 -0300

    lecture 59

--- exercicio05/app.js
+++ exercicio05/app.js
@@ -3,12 +3,17 @@
 // The array can be changed as long as the reference is the same.
 // We can changed because we're not updating or altering this
 // reference.
+
 const myEggs = ['brown','brown'];
 myEggs[0] = 'amazing';
 myEggs.push('nice!');
+
 
 // We cant changed the reference, like this:
 /*
     myEggs = ['brown','pink']; (ERRO)
 
 */
+
+
+
---
+++ exercicio05/lecture59.js
@@ -0,0 +1,13 @@
+//Lecture 59. Creating Objects Literals
+
+const fitBitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep: '2:13', 
+}
+// When we deal with objects, we declare as const.
+
+// All keys are converted to string. 
+//      -> Ex: totalSteps are string. 
