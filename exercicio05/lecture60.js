05b010a05136d234ffe0cc5e3a06072141304107
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2019-12-26 01:51:16 -0300

    lecture 60 - acessing object properties

---
+++ exercicio05/lecture60.js
@@ -0,0 +1,9 @@
+const numbers = {
+    100: 'one hundred',
+    16: 'sixteen'
+};
+
+/* For acessing properties of objects we use '.' but, if the key are number,
+ we use '[]',  example:   */
+
+ console.log(numbers[100]);
