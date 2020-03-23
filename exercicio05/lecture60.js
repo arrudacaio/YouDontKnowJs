34a5a13105fdc6427280918af4e347baa81bca16
arrudacaio <caio.arruda@ccc.ufcg.edu.br>
2020-03-23 02:35:12 -0300

    lexical scope

--- exercicio05/lecture60.js
+++
@@ -1,9 +0,0 @@
-const numbers = {
-    100: 'one hundred',
-    16: 'sixteen'
-};
-
-/* For acessing properties of objects we use '.' but, if the key are number,
- we use '[]',  example:   */
-
- console.log(numbers[100]);
---
+++ nestedFunctions.js
@@ -0,0 +1,18 @@
+/**
+ * Quando temos funções aninhadas, a função que está dentro
+ * tem acesso as variaveis que estão na função mais externa
+ *
+ * Porém se eu declarar uma variável na função interna, a externa não
+ * consegue acessa-la
+ */
+
+function outer() {
+  let movie = "O poço";
+
+  function inner() {
+    let x = 10;
+    console.log(movie.toUpperCase());
+  }
+  inner();
+  //console.log(x * 2);
+}
